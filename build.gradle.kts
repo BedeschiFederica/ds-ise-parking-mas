8bf47a582870a0ff574d0676a552d575205bee36
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2026-08-17 15:04:25 +0200

    build: setup gradle project and add run tasks

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets {
+    main {
+        resources {
+            srcDir("src/main/asl")
+        }
+    }
+}
+
+dependencies {
+    implementation("io.github.jason-lang:jason-interpreter:3.2.1") // Java 17
+    testImplementation("junit", "junit", "4.13.2")
+}
+
+tasks.register<JavaExec>("runParkingMain") {
+    group = "run"
+    classpath = sourceSets.getByName("main").runtimeClasspath
+    mainClass.set("jason.infra.jade.RunJadeMAS")
+    args(file("parkingMain.mas2j").absolutePath)
+    javaLauncher.set(javaToolchains.launcherFor(java.toolchain))
+}
+
+tasks.register<JavaExec>("runParkingContainer") {
+    group = "run"
+    val containerName = providers.gradleProperty("name").orNull
+        ?: throw GradleException("Missing required property: -Pname=<container-name>")
+    classpath = sourceSets.getByName("main").runtimeClasspath
+    mainClass.set("jason.infra.jade.RunJadeMAS")
+    args(
+        file("parkingAgents.mas2j").absolutePath,
+        "-container",
+        "-container-name", containerName,
+        "-host", "localhost"
+    )
+    javaLauncher.set(javaToolchains.launcherFor(java.toolchain))
+}
