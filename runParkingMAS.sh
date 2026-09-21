5e48d3bba952aa4420948fde0b1a509802a9323e
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2026-09-21 12:34:52 +0200

    build: add automatic shutdown to MAS run script

--- runParkingMAS.sh
+++ runParkingMAS.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 ./gradlew runParkingMain &
+MAIN_PID=$!
+
 sleep 2
 
 ./gradlew runParkingContainer -Pname=area1 &
+AREA1_PID=$!
 ./gradlew runParkingContainer -Pname=area2 &
+AREA2_PID=$!
 ./gradlew runParkingContainer -Pname=drivers &
+DRIVERS_PID=$!
 
+wait "$MAIN_PID"
+kill "$AREA1_PID" "$AREA2_PID" "$DRIVERS_PID" 2>/dev/null
 wait
